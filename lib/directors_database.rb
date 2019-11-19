6f346d06bcd49738aaa3a7d48c9c15387b8ee289
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-19 00:18:36 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -2,3 +2,17 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+ gross_for directors
+   = {"Stephen Spielberg"=>1357566430,
+        "Russo Brothers"=>2281002470,
+        "James Cameron"=>2571020373,
+        "Spike Lee"=>256624217,
+        "Wachowski Siblings"=>806180282,
+        "Robert Zemeckis"=>1273838385,
+        "Quentin Tarantino"=>662738268,
+        "Martin Scorsese"=>636812242,
+        "Francis Ford Coppola"=>509719258}
+def gross_for_director(director_data)
+  gross_for directors.sum
+end
